6aded6ef1e7f965943bd5c7fc36c234c128de7c0
sbx_user1051 <sbx_user1051@169.254.98.7>
2025-04-21 15:55:28 +0000

    Mon Apr 21 15:55:28 UTC 2025

--- test/solution_tests/CHK/test_CHK_solution.py
+++ test/solution_tests/CHK/test_CHK_solution.py
@@ -6,8 +6,12 @@ class TestHLO():
         tester = CheckoutSolution()
         assert tester.checkout("ABCCDE") == 175
         assert tester.checkout("AAAAAAAAABBBBBCD") == 535
-        assert tester.checkout("gfjhb") == -1
+        assert tester.checkout("sfdgdf") == -1
         assert tester.checkout("BBBBBEEEE") == 235
         assert tester.checkout("FFFFF") == 40
         assert tester.checkout("HHHHHHHHHHHHHHHH") == 160
         assert tester.checkout("KKK") == 230
+        assert tester.checkout("MMMNNNN") == 190
+        assert tester.checkout("PPPPPP") == 250
+        assert tester.checkout("QQQQRRRR") == 280
+        assert tester.checkout("UUUU") == 120
