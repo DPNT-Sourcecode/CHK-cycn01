941ed4c10e038291b6d25d34f7960c324032b2f8
sbx_user1051 <sbx_user1051@169.254.98.7>
2025-04-21 14:07:14 +0000

    Mon Apr 21 14:07:14 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,16 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        raise NotImplementedError()
+        # create dict of skus to consolidate them
+        sku_dict = dict()
+        for sku in skus:
+            # get old and + 1
+            if sku_dict.get(sku) is None:
+                sku_dict.update([sku, 1])
+            else:
+                val = sku_dict.get(sku)
+                sku_dict.update([sku, val+1])
+                
+        #check sku_dict is as expected
+        print(sku_dict)
+
