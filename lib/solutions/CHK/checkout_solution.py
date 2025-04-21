17885ec4367d3406d7439767b624c57e6ce5324c
sbx_user1051 <sbx_user1051@169.254.98.7>
2025-04-21 15:34:28 +0000

    Mon Apr 21 15:34:28 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -42,36 +42,32 @@ class CheckoutSolution:
             match key:
                 case "A":
                     number = sku_dict.get(key)
-                    price += self.two_tier_discount(A_price, A_special, A_special_2, number)
+                    price += self.two_tier_discount(prices["A"], A_special, A_special_2, number)
                 case "B":
                     number = sku_dict.get(key)
-                    price += self.get_free_by_other(B_price, B_special, E_special, number, sku_dict.get("E"))
-                    # # Apply the E discount by taking away any Bs that are made free by the Es
-                    # if sku_dict.get("E") is not None:
-                    #     no_of_Es = sku_dict.get("E")
-                    #     number = number - (no_of_Es // E_special)
-                    # # Now we've gotten rid of the free Bs, we can continue as normal
-                    # number_of_non_specials = number % B_special[0]
-                    # price += number_of_non_specials * B_price
-                    # price += ((number - number_of_non_specials)/B_special[0]) * B_special[1]
+                    price += self.get_free_by_other(prices["B"], B_special, E_special, number, sku_dict.get("E"))
                 case "C":
-                    price += sku_dict.get(key) * C_price
+                    price += sku_dict.get(key) * prices["C"]
                 case "D":
-                    price += sku_dict.get(key) * D_price
+                    price += sku_dict.get(key) * prices["D"]
                 case "E":
                     # E specials have been applied in B case.
-                    price += sku_dict.get(key) * E_price
+                    price += sku_dict.get(key) * prices["E"]
                 case "F":
                     #buy 2F get 1F free, but only in 3s.
                     # This is basically the same as the basic B case, because 3Fs = a new price, and any remainders are added as normal.
                     number = sku_dict.get(key)
-                    number_of_non_specials = number % F_special[0]
-                    price += number_of_non_specials * F_price
-                    price += ((number - number_of_non_specials)/F_special[0]) * F_special[1]
+                    price += self.basic_special(prices["F"], F_special, number)
                 case _:
                     return -1
                 
         return int(price)
+    
+    def basic_special(self, price, special, number):
+        number_of_non_specials = number % special[0]
+        out_price = number_of_non_specials * price
+        out_price += ((number - number_of_non_specials)/special[0]) * special[1]
+        return out_price
     
     def two_tier_discount(self, price, special1, special2, number):
         # mod the larger special to get remainder
@@ -96,6 +92,7 @@ class CheckoutSolution:
         out_price = number_of_non_specials * price
         out_price += ((number - number_of_non_specials)/special[0]) * special[1]
         return out_price
+
 
 
 
