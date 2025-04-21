16a91f94da48b32fff3640c739ab6d73c17c610c
sbx_user1051 <sbx_user1051@169.254.98.7>
2025-04-21 15:46:28 +0000

    Mon Apr 21 15:46:28 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -16,14 +16,33 @@ class CheckoutSolution:
         #check sku_dict is as expected
         print(sku_dict)
 
-        #set up base prices
+        #set up base prices    
         prices = {
-            "A": 50,
-            "B": 30,
+            "A": 50, #3A for 130, 5A for 200
+            "B": 30, #2B for 45
             "C": 20,
             "D": 15,
-            "E": 40,
-            "F": 10
+            "E": 40, #2E get one B free
+            "F": 10, #2F get one F free
+            "G": 20,
+            "H": 35, #5H for 45, 10H for 80
+            "I": 60,
+            "K": 80, #2K for 150    
+            "L": 90,
+            "M": 15,
+            "N": 40, #3N get one M free
+            "O": 10,
+            "P": 50, #5P for 200
+            "Q": 30, #3Q for 80
+            "R": 50, #3R get one Q free
+            "S": 30,
+            "T": 20,
+            "U": 40, #3U get one U free
+            "V": 50, #2V for 90, 3V for 130
+            "W": 20,
+            "X": 90,
+            "Y": 10,
+            "Z": 50
         }
 
         #set up specials
@@ -32,6 +51,16 @@ class CheckoutSolution:
         B_special = [2, 45]
         E_special = 2
         F_special = [3, prices["F"] * 2]
+        H_special = [5, 45]
+        H_special_2 = [10, 80]
+        K_special = [2, 150]
+        N_special = 3
+        P_special = [5, 200]
+        Q_special = [3, 80]
+        R_special = 3
+        U_special = [3, prices["U"] * 3]
+        V_special = [2, 90]
+        V_special_2 = [3, 130]
 
         #calculate the price
         price = 0
@@ -42,22 +71,66 @@ class CheckoutSolution:
             match key:
                 case "A":
                     number = sku_dict.get(key)
-                    price += self.two_tier_discount(prices["A"], A_special, A_special_2, number)
+                    price += self.two_tier_discount(prices[key], A_special, A_special_2, number)
                 case "B":
                     number = sku_dict.get(key)
-                    price += self.get_free_by_other(prices["B"], B_special, E_special, number, sku_dict.get("E"))
+                    price += self.get_free_by_other(prices[key], B_special, E_special, number, sku_dict.get("E"))
                 case "C":
-                    price += sku_dict.get(key) * prices["C"]
+                    price += sku_dict.get(key) * prices[key]
                 case "D":
-                    price += sku_dict.get(key) * prices["D"]
+                    price += sku_dict.get(key) * prices[key]
                 case "E":
                     # E specials have been applied in B case.
-                    price += sku_dict.get(key) * prices["E"]
+                    price += sku_dict.get(key) * prices[key]
                 case "F":
                     #buy 2F get 1F free, but only in 3s.
                     # This is basically the same as the basic B case, because 3Fs = a new price, and any remainders are added as normal.
                     number = sku_dict.get(key)
-                    price += self.basic_special(prices["F"], F_special, number)
+                    price += self.basic_special(prices[key], F_special, number)
+                case "G":
+                    price += sku_dict.get(key) * prices[key]
+                case "H":
+                    #5H for 45, 10H for 80
+                    number = sku_dict.get(key)
+                    price += self.two_tier_discount(prices[key], H_special, H_special_2, number)
+                case "I":
+                    price += sku_dict.get(key) * prices[key]
+                case "J":
+                    price += sku_dict.get(key) * prices[key]
+                case "K":
+                    #2K for 150  
+                    number = sku_dict.get(key)
+                    price += self.basic_special(prices[key], K_special, number)
+                case "L":
+                    price += sku_dict.get(key) * prices[key]
+                case "M":
+                    price += sku_dict.get(key) * prices[key]
+                case "N":
+                    price += sku_dict.get(key) * prices[key]
+                case "O":
+                    price += sku_dict.get(key) * prices[key]
+                case "P":
+                    price += sku_dict.get(key) * prices[key]
+                case "Q":
+                    price += sku_dict.get(key) * prices[key]
+                case "R":
+                    price += sku_dict.get(key) * prices[key]
+                case "S":
+                    price += sku_dict.get(key) * prices[key]
+                case "T":
+                    price += sku_dict.get(key) * prices[key]
+                case "U":
+                    price += sku_dict.get(key) * prices[key]
+                case "V":
+                    price += sku_dict.get(key) * prices[key]
+                case "W":
+                    price += sku_dict.get(key) * prices[key]
+                case "X":
+                    price += sku_dict.get(key) * prices[key]
+                case "Y":
+                    price += sku_dict.get(key) * prices[key]
+                case "Z":
+                    price += sku_dict.get(key) * prices[key]
                 case _:
                     return -1
                 
@@ -92,8 +165,3 @@ class CheckoutSolution:
         out_price = number_of_non_specials * price
         out_price += ((number - number_of_non_specials)/special[0]) * special[1]
         return out_price
-
-
-
-
-
